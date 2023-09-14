cc86e6e00e28fd6a49bcc8821e5128e630abb1ed
Bozhikov99 <72099913+Bozhikov99@users.noreply.github.com>
2023-09-14 22:16:35 +0300

    Adding homework.

---
+++ 01. Linear Data Structures/Lab/Problem01.List/List.cs
@@ -0,0 +1,158 @@
+﻿namespace Problem01.List
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class List<T> : IAbstractList<T>
+    {
+        private const int DEFAULT_CAPACITY = 4;
+        private T[] items;
+
+        public List()
+            : this(DEFAULT_CAPACITY)
+        {
+            items = new T[DEFAULT_CAPACITY];
+        }
+
+        public List(int capacity)
+        {
+            items = new T[capacity];
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                ValidateIndex(index);
+                return items[index];
+            }
+            set
+            {
+                ValidateIndex(index);
+                items[index] = value;
+            }
+        }
+
+        public int Count { get; private set; }
+
+        public void Add(T item)
+        {
+            if (Count == items.Length)
+            {
+                Grow();
+            }
+
+            items[Count++] = item;
+        }
+
+        public bool Contains(T item)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (items[i].Equals(item))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return items[i];
+            }
+        }
+
+        public int IndexOf(T item)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (items[i].Equals(item))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public void Insert(int index, T item)
+        {
+            ValidateIndex(index);
+            ShiftRight(index);
+
+            items[index] = item;
+            Count++;
+        }
+
+        public bool Remove(T item)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (items[i].Equals(item))
+                {
+                    Count--;
+                    ShiftLeft(i);
+
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public void RemoveAt(int index)
+        {
+            ValidateIndex(index);
+
+            Count--;
+            ShiftLeft(index);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private void Grow()
+        {
+            int length = items.Length * 2;
+            T[] newArray = new T[length];
+
+            Array.Copy(items, newArray, items.Length);
+            items = newArray;
+        }
+
+        private void ValidateIndex(int index)
+        {
+            if (index >= Count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            if (index < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+        }
+
+        private void ShiftLeft(int index)
+        {
+            for (int i = index; i < Count; i++)
+            {
+                items[i] = items[i + 1];
+            }
+        }
+
+        private void ShiftRight(int index)
+        {
+            for (int i = Count; i > index; i--)
+            {
+                items[i] = items[i - 1];
+            }
+        }
+    }
+}
