843888b38a71cd5e1f6d1ecfc8a3a9a5a59b1840
Bozhikov99 <72099913+Bozhikov99@users.noreply.github.com>
2023-09-15 13:32:10 +0300

    Adding exercise.

---
+++ 01. Linear Data Structures/Lab/04. Singly linked list/SinglyLinkedList.cs
@@ -0,0 +1,160 @@
+﻿namespace Problem04.SinglyLinkedList
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class SinglyLinkedList<T> : IAbstractLinkedList<T>
+    {
+        public class Node
+        {
+            public Node(T value)
+            {
+                Value = value;
+            }
+
+            public Node Next { get; set; }
+
+            public T Value { get; set; }
+
+            public bool HasNext()
+            {
+                return Next != null;
+            }
+        }
+
+        private int count;
+
+        public int Count => count;
+
+        public Node Head { get; set; }
+
+        public Node Tail { get; set; }
+
+        public void AddFirst(T item)
+        {
+            Node node = new Node(item);
+
+            if (Count == 0)
+            {
+                Head = node;
+                Tail = node;
+            }
+            else
+            {
+                node.Next = Head;
+                Head = node;
+            }
+
+            count++;
+        }
+
+        public void AddLast(T item)
+        {
+            Node node = new Node(item);
+
+            if (Count == 0)
+            {
+                Head = node;
+                Tail = node;
+            }
+            else
+            {
+                Node current = Head;
+
+                while (current.HasNext())
+                {
+                    current = current.Next;
+                }
+
+                current.Next = node;
+                Tail = node;
+            }
+
+            count++;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node current = Head;
+
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
+        }
+
+        public T GetFirst()
+        {
+            EnsureNotNull();
+
+            T value = Head.Value;
+
+            return value;
+        }
+
+        public T GetLast()
+        {
+            EnsureNotNull();
+
+            Node current = Tail;
+            T value = current.Value;
+
+            return value;
+        }
+
+        public T RemoveFirst()
+        {
+            EnsureNotNull();
+
+            T value = Head.Value;
+            Head = Head.Next;
+            count--;
+
+            return value;
+        }
+
+        public T RemoveLast()
+        {
+            EnsureNotNull();
+
+            Node current = Head;
+
+            if (!current.HasNext())
+            {
+                T headValue = Head.Value;
+
+                Head = null;
+                count--;
+
+                return headValue;
+            }
+
+            while (current.Next != Tail)
+            {
+                current = current.Next;
+            }
+
+            T value = Tail.Value;
+            Tail = current;
+            Tail.Next = null;
+            count--;
+
+            return value;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private void EnsureNotNull()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+    }
+}
