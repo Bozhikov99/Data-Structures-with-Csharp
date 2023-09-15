82bd479572acff2f20e92d6efc31774990580dda
Bozhikov99 <72099913+Bozhikov99@users.noreply.github.com>
2023-09-15 13:56:47 +0300

    Adding exercise.

---
+++ 01. Linear Data Structures/Exercise/04. Balanced parentheses/BalancedParenthesesSolve.cs
@@ -0,0 +1,56 @@
+﻿
+namespace Problem04.BalancedParentheses
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class BalancedParenthesesSolve : ISolvable
+    {
+        public bool AreBalanced(string parentheses)
+        {
+            //({[]}) ()[]{}
+
+            if (parentheses.Length % 2 != 0)
+            {
+                return false;
+            }
+
+            Stack<char> symbols = new Stack<char>();
+
+            for (int i = 0; i < parentheses.Length; i++)
+            {
+                char expected = default;
+                char current = parentheses[i];
+
+                switch (current)
+                {
+                    case ')':
+                        expected = '(';
+                        break;
+                    case ']':
+                        expected = '[';
+                        break;
+                    case '}':
+                        expected = '{';
+                        break;
+                    default:
+                        symbols.Push(current);
+                        break;
+                }
+
+                if (expected == default(char))
+                {
+                    continue;
+                }
+
+                if (symbols.Pop() != expected)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
