a972a9542e4f5ab7373d07a0c3218f2b7c95b8a4
Bozhikov99 <72099913+Bozhikov99@users.noreply.github.com>
2023-09-15 15:58:41 +0300

    Adding exercise.

---
+++ 02. Trees representation and traversal (BFS, DFS)/Exercise/Tree/IntegerTreeFactory.cs
@@ -0,0 +1,71 @@
+﻿namespace Tree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+
+    public class IntegerTreeFactory
+    {
+        private Dictionary<int, IntegerTree> nodesByKey;
+
+        public IntegerTreeFactory()
+        {
+            this.nodesByKey = new Dictionary<int, IntegerTree>();
+        }
+
+        public IntegerTree CreateTreeFromStrings(string[] input)
+        {
+            foreach (string s in input)
+            {
+                int[] keys = s.Split(' ')
+                    .Select(int.Parse)
+                    .ToArray();
+
+                AddEdge(keys[0], keys[1]);
+            }
+
+            return GetRoot();
+        }
+
+        public IntegerTree CreateNodeByKey(int key)
+        {
+            if (!nodesByKey.ContainsKey(key))
+            {
+                IntegerTree node = new IntegerTree(key);
+                nodesByKey.Add(key, node);
+
+                return node;
+            }
+            else
+            {
+                IntegerTree node = nodesByKey[key];
+
+                return node;
+            }
+
+        }
+
+        public void AddEdge(int parent, int child)
+        {
+            IntegerTree parentNode = CreateNodeByKey(parent);
+            IntegerTree childNode = CreateNodeByKey(child);
+
+            parentNode.AddChild(childNode);
+            childNode.AddParent(parentNode);
+        }
+
+        public IntegerTree GetRoot()
+        {
+            foreach (IntegerTree n in nodesByKey.Values)
+            {
+                if (n.Parent is null)
+                {
+                    return n;
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ 02. Trees representation and traversal (BFS, DFS)/Exercise/Tree/Tree.cs
@@ -0,0 +1,81 @@
+﻿namespace Tree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Tree<T> : IAbstractTree<T>
+    {
+        private List<Tree<T>> children;
+
+        public Tree(T key, params Tree<T>[] children)
+        {
+            Key = key;
+
+            this.children = new List<Tree<T>>();
+
+            foreach (Tree<T> c in children)
+            {
+                this.children.Add(c);
+            }
+        }
+
+        public T Key { get; private set; }
+
+        public Tree<T> Parent { get; private set; }
+
+        public IReadOnlyCollection<Tree<T>> Children => children.AsReadOnly();
+
+        public void AddChild(Tree<T> child)
+        {
+            children.Add(child);
+        }
+
+        public void AddParent(Tree<T> parent)
+        {
+            Parent = parent;
+        }
+
+        public string AsString()
+        {
+            StringBuilder builder = new StringBuilder();
+
+            DfsString(this, 0, builder);
+            string output = builder.ToString()
+                            .Trim();
+
+            return output;
+        }
+
+        private void DfsString(Tree<T> tree, int identationLevel, StringBuilder builder)
+        {
+            builder.Append(' ', identationLevel++ * 2);
+            builder.AppendLine($"{tree.Key}");
+
+            foreach (Tree<T> c in tree.children)
+            {
+                DfsString(c, identationLevel, builder);
+            };
+        }
+
+        public IEnumerable<T> GetInternalKeys()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetLeafKeys()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetDeepestKey()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetLongestPath()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
