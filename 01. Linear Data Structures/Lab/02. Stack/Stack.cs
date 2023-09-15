63fc60a93ad6dd1a9eb9fce21588d96c57bc3587
Bozhikov99 <72099913+Bozhikov99@users.noreply.github.com>
2023-09-15 13:27:58 +0300

    Adding exercise.

---
+++ 01. Linear Data Structures/Lab/02. Stack/Stack.cs
@@ -0,0 +1,96 @@
+﻿namespace Problem02.Stack
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class Stack<T> : IAbstractStack<T>
+    {
+        private class Node
+        {
+            public Node(T value)
+            {
+                Value = value;
+            }
+
+            public T Value { get; set; }
+
+            public Node Next { get; set; }
+        }
+
+        private Node top;
+        private int count;
+
+        public int Count => count;
+
+        public void Push(T item)
+        {
+            Node newNode = new Node(item);
+            newNode.Next = top;
+            top = newNode;
+
+            count++;
+        }
+
+        public T Pop()
+        {
+            EnsureNotEmpty();
+
+            Node currentTop = top;
+            top = currentTop.Next;
+            count--;
+
+            return currentTop.Value;
+        }
+
+        public T Peek()
+        {
+            EnsureNotEmpty();
+
+            T value = top.Value;
+
+            return value;
+        }
+
+        public bool Contains(T item)
+        {
+            Node current = top;
+
+            while (current != null)
+            {
+                if (current.Value.Equals(item))
+                {
+                    return true;
+                }
+
+                current = current.Next;
+            }
+
+            return false;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node current = top;
+
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private void EnsureNotEmpty()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+    }
+}
