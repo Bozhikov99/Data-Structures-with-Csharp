9fa87bcf29eac78172b694900653e38a007445d2
Bozhikov99 <72099913+Bozhikov99@users.noreply.github.com>
2023-09-15 13:57:48 +0300

    Adding exercise.

---
+++ 01. Linear Data Structures/Exercise/05. Calculate Sequence/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Channels;
+
+namespace _06.CalculateSequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Queue<int> queue = new Queue<int>();
+
+            queue.Enqueue(n);
+
+            while (queue.Count < 50)
+            {
+                queue.Enqueue(n + 1);
+                queue.Enqueue(2 * n + 1);
+                queue.Enqueue(n + 2);
+
+                n+= 1;
+            }
+
+            Console.WriteLine(string.Join(", ", queue));
+        }
+    }
+}
